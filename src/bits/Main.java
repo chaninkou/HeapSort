5e2f4a97fffae8aaf635d16b14196b4430b9681d
chaninkou <michaelkoulove@yahoo.com>
2020-03-05 22:56:34 -0800

    Forgot to put main method, good question on dynamic programming.

--- src/bits/Main.java
+++ src/bits/Main.java
@@ -1,5 +1,15 @@
 package bits;
 
-public class Main {
+import java.util.Arrays;
 
+public class Main {
+	public static void main(String[] args) {
+		int num = 15;
+		
+		System.out.println("Input: " + num);
+		
+		CountBitsInArrayFunction solution = new CountBitsInArrayFunction();
+		
+		System.out.println("Solution: " + Arrays.toString(solution.countBits(num)));
+	}
 }
